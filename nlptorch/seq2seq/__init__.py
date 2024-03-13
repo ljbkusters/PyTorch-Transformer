b89cb030d587b696a9adc5b575a121da3535b7aa
Luc Kusters <luc.kusters@anatech.nl>
2024-03-13 10:51:07 +0100

    nlptorch.seq2seq.__init__: Add config imports

--- nlptorch/seq2seq/__init__.py
+++ nlptorch/seq2seq/__init__.py
@@ -1,10 +1,16 @@
 from nlptorch.seq2seq._modules import Seq2Seq
+from nlptorch.seq2seq._modules import Seq2SeqConfig
 from nlptorch.seq2seq._modules import S2SEncoder
+from nlptorch.seq2seq._modules import S2SEncoderConfig
 from nlptorch.seq2seq._modules import S2SDecoder
+from nlptorch.seq2seq._modules import S2SDecoderConfig
 
 
 __all__ = [
     Seq2Seq.__name__,
+    Seq2SeqConfig.__name__,
     S2SEncoder.__name__,
+    S2SEncoderConfig.__name__,
     S2SDecoder.__name__,
+    S2SDecoderConfig.__name__,
 ]
